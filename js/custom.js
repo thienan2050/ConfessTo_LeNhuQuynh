520d13830a4b89e7b814ff1d5f4b905e912802f6
Bui Thien An <an.bui-thien@internal.banvien.com>
2023-01-03 01:24:02 +0700

    Adding some notes

--- js/custom.js
+++ js/custom.js
@@ -113,7 +113,7 @@
     Countdown Clock
     ................................................. */
 	function makeTimer() {
-		var endTime = new Date("09 Jan 2023 17:30:00 GMT+07:00");			
+		var endTime = new Date("07 Jan 2023 17:00:00 GMT+07:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
